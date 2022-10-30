9fcbf5cd1bbb71f066360f75764d2727d3d98b38
Kevin <kevintimachy@gmail.com>
2022-10-30 11:52:54 -0400

    assignment3

--- data-server.js
+++ data-server.js
@@ -62,3 +62,57 @@ exports.getDepartments = function() {
     });
 }
 
+exports.addEmployee = function (employeeData) {
+    return new Promise((resolve, reject) => {
+        if (employeeData.isManager == undefined) {
+            employeeData.isManager = true;
+        }
+        employeeData.employeeNum = employees.length + 1;
+        employees.push(employeeData);
+        if (JSON.stringify(employees[employees.length]) === employeeData)
+            resolve();
+        else
+            reject("Did not add employee properly");
+        
+    });
+}
+
+exports.getEmployeesByStatus = function (status) {
+    return new Promise((resolve, reject) => {
+        if (employees.filter(emps => emps.status === status).length > 0) {
+            resolve(employees.filter(emps => emps.status === status));
+        }
+        else
+            reject("no results returned");
+    });
+}
+
+exports.getEmployeesByDepartment = function (department) {
+    return new Promise((resolve, reject) => {
+        if (employees.filter(emps => emps.department === parseInt(department)).length > 0) {
+            resolve(employees.filter(emps => emps.department == parseInt(department)));
+        }
+        else
+            reject("no results returned");
+    });
+}
+
+exports.getEmployeesByManager = function (manager) {
+    return new Promise((resolve, reject) => {
+        if (employees.filter(emps => emps.employeeManagerNum === parseInt(manager)).length > 0) {
+            resolve(employees.filter(emps => emps.employeeManagerNum === parseInt(manager)));
+        }
+        else
+            reject("no results returned");
+    });
+}
+
+exports.getEmployeeByNum = function (num) {
+    return new Promise((resolve, reject) => {
+        if (employees.find(emps => emps.employeeNum === parseInt(num))) {
+            resolve(employees.find(emps => emps.employeeNum === parseInt(num)));
+        }
+        else
+            reject("no results returned");
+    });
+}
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // http://localhost:8080/
 /*************************************************************************
-* WEB322– Assignment 2
+* WEB322– Assignment 3
 * I declare that this assignment is my own work in accordance with Seneca Academic
 Policy. No part * of this assignment has been copied manually or electronically from any
 other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Kevin Timachy Student ID: 145075180 Date: 02/10/22
+* Name: Kevin Timachy Student ID: 145075180 Date: 06/10/22
 *
-* Your app’s URL (from Cyclic) :
+* Your app’s URL (from Cyclic) : https://lime-thankful-wildebeest.cyclic.app 
 *
 *************************************************************************/ 
 
@@ -16,11 +16,26 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var data_server = require("./data-server.js");
+var multer = require("multer");
+const fs = require('fs');
+const { INSPECT_MAX_BYTES } = require("buffer");
+
 
 
 var HTTP_PORT = process.env.PORT || 8080
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
 
 
 app.get("/", function (req, res) {
@@ -32,7 +47,22 @@ app.get("/about", function (req, res) {
 });
 
 app.get("/employees", function (req, res) {
-    data_server.getAllEmployees().then(result => res.json(result)).catch(error => res.json(error));
+    if (req.query.status) {
+        data_server.getEmployeesByStatus(req.query.status).then(result => res.json(result)).catch(err => res.json(err));
+    }
+    else if (req.query.department) {
+        data_server.getEmployeesByDepartment(req.query.department).then(result => res.json(result)).catch(err => res.json(err));
+    }
+    else if (req.query.manager) {
+        data_server.getEmployeesByManager(req.query.manager).then(result => res.json(result)).catch(err => res.json(err));
+    }
+    else {
+        data_server.getAllEmployees().then(result => res.json(result)).catch(error => res.json(error));
+    }
+});
+
+app.get("/employee/:value", (req, res) =>{
+    data_server.getEmployeeByNum(req.params.value).then(result => res.json(result)).catch(error => res.json(error));
 });
 
 app.get("/managers", function (req, res) {
@@ -42,6 +72,35 @@ app.get("/managers", function (req, res) {
 app.get("/departments", function (req, res) {
     data_server.getDepartments().then(result => res.json(result)).catch(error => res.json(error));
 });
+
+app.get("/employees/add", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/addEmployee.html"));
+});
+
+app.post("/employees/add", (req, res) =>
+{
+    data_server.addEmployee(req.body).then(res.redirect("/employees")).catch(err => { res.json(err); })
+});
+
+app.get("/images/add", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/addImage.html"));
+});
+
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+    res.redirect("/images");
+});
+
+app.get("/images", (req, res) => {
+ 
+    fs.readdir("./public/images/uploaded", (err, items) => {
+        
+
+        res.json({ images: items });
+        
+    });
+   
+});
+
 
 app.use(function (req, res) {
     res.status(404).sendFile(path.join(__dirname,"./views/404.html"));
@@ -55,4 +114,3 @@ function onHttpStart()
 
 data_server.initialize().then(() => { app.listen(HTTP_PORT, onHttpStart); }).catch(() => { console.log("Data not initialized cannot start app"); });
 
-//app.listen(HTTP_PORT, onHttpStart);
